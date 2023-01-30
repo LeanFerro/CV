dc8fab5bc50e3026309e3c54defd29e2a6fa57e4
Leandro Ferro <fesin81@gmail.com>
2023-01-30 20:07:24 -0300

    arrgelos botones

--- cambios.js
+++ cambios.js
@@ -1,4 +1,22 @@
 
 document.getElementById('bot1').addEventListener('click', function() {
-    document.getElementById('divbot1').style.display = "none";
+    document.getElementById('bot1').style.display = "none";
+    document.getElementById('bot2').style.display = "initial"; 
 })
+
+document.getElementById('bot2').addEventListener('click', function() {
+
+    document.getElementById('bot2').style.display = "none";
+    document.getElementById('bot3').style.display = "initial";  
+})
+
+document.getElementById('bot3').addEventListener('click', function() {
+    document.getElementById('bot3').style.display = "none";
+    document.getElementById('bot4').style.display = "initial";
+})
+
+document.getElementById('bot4').addEventListener('click', function() {
+    document.getElementById('bot1').style.display = "initial";
+    document.getElementById('bot4').style.display = "none";   
+})
+
